ba2fd3245214f3d27134b4aef570d01036eece64
Ben Hirsch <benhirsch42@gmail.com>
2018-11-28 23:02:16 -0500

    Start porting to apollo

--- config/prod.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-
-  pouchDB: {
-    remote: 'http://localhost:5984/fatedb',
-  }
-}
--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <template>
   <div id="app">
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ApolloExample msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
+import ApolloExample from './components/ApolloExample.vue'
 
-  created: function() {
-    this.$pouch.sync('fatedb', 'http://10.0.0.167:5984/fatedb');
-    // this.$pouch.sync('fatedb', 'localhost:5984/fatedb');
+export default {
+  name: 'app',
+  components: {
+    ApolloExample
   }
 }
 </script>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Space+Mono');
-@import 'styles/variables';
-
-body {
-  background-color: $color-bg;
-  color: $color-text;
-  font-family: Space Mono;
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-
-input {
-  background: none !important;
-  border: none;
-  font-family: Space Mono;
-  color: $color-text;
-  padding: 0;
-  margin: 0;
-}
-
-textarea {
-  width: 100%;
-  background: none !important;
-  border: none;
-  font-family: Space Mono;
-  color: $color-text;
-  padding: 0;
-  margin: 0;
-  font-size: 20px;
-  height: 0px;
-}
-
 </style>
